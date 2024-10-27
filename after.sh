f81765289d948fad7bc08221869ef5ac5f58a20d
Anton <mainsample@ya.ru>
2024-10-27 18:29:12 +0300

    fix: edit install packages

--- after.sh
+++ after.sh
@@ -10,14 +10,18 @@ setfont cyr-sun16
 echo LANG=en_US.UTF-8 >> /etc/locale.conf
 echo anton-pc > /etc/hostname
 sudo timedatectl set-timezone Europe/Moscow
-pacman -S grub efibootmgr networkmanager openssh vim code firefox mattermost-desktop htop wget docker docker-compose sudo pipewire ly kitty hyprland waybar hyprpaper otf-font-awesome ttf-arimo-nerd noto-fonts
+pacman -S grub efibootmgr networkmanager openssh git vim firefox mattermost-desktop htop wget docker docker-compose sudo pipewire ly kitty hyprland waybar hyprpaper otf-font-awesome ttf-arimo-nerd noto-fonts base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+yay -S visual-studio-code-bin
 
 systemctl enable NetworkManager
 systemctl enable ly
 systemctl enable docker
 
 echo %wheel ALL=(ALL) ALL >> /etc/sudoers
-useradd -m anton -G wheel
+useradd -m anton -G wheel,docker -u 1000
 
 # grub-install /dev/sda --efi-dir=/boot/
 # grub-mkconfig -o /boot/grub/grub.cfg
