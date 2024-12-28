d2b6597ac709fcdd36277ed2d4c1ece711364b48
Anton Panteleev <mainsample@ya.ru>
2024-12-28 18:43:32 +0300

    feat: any services, change hyperland conf

--- after.sh
+++ after.sh
@@ -10,18 +10,29 @@ setfont cyr-sun16
 echo LANG=en_US.UTF-8 >> /etc/locale.conf
 echo anton-pc > /etc/hostname
 sudo timedatectl set-timezone Europe/Moscow
-pacman -S grub efibootmgr networkmanager openssh git vim firefox mattermost-desktop htop wget docker docker-compose sudo ly kitty hyprland waybar hyprpaper otf-font-awesome ttf-arimo-nerd noto-fonts base-devel pipewire pipewire-pulse pipewire-media-session bluez bluez-utils
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-yay -S visual-studio-code-bin
+pacman -S grub efibootmgr networkmanager openssh \ 
+            git vim firefox mattermost-desktop htop \
+            wget docker docker-compose sudo \
+            ly kitty hyprland waybar hyprpaper  dolphin wofi \
+            otf-font-awesome ttf-arimo-nerd noto-fonts base-devel \
+            pipewire pipewire-pulse pipewire-media-session rtkit alsa-utils pavucontrol \
+            bluez bluez-utils blueman
 
 systemctl enable NetworkManager
 systemctl enable ly
 systemctl enable docker
+systemctl enable pipewire.service
+systemctl enable pipewire-pulse.service
+systemctl enable rtkit-daemon.service
+systemctl enable bluetooth
 
 echo %wheel ALL=(ALL) ALL >> /etc/sudoers
 useradd -m anton -G wheel,docker -u 1000
+
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+yay -S visual-studio-code-bin
 
 # grub-install /dev/sda --efi-dir=/boot/
 # grub-mkconfig -o /boot/grub/grub.cfg
